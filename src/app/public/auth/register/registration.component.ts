b29d6616fa7ddc697d96316742cf88802bdd77dd
Dany <dany.yacoub@gmail.com>
2020-08-08 13:36:58 +0300

    add password to registration form

--- src/app/public/auth/register/registration.component.ts
+++ src/app/public/auth/register/registration.component.ts
@@ -3,57 +3,69 @@ import {Router} from '@angular/router';
 import {UserRegistrationService} from '../../../service/user-registration.service';
 import {CognitoCallback} from '../../../service/cognito.service';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {MinLengthValidator} from '../../validators/min-length.validator';
+import {ConfirmPasswordValidator} from '../../validators/confirm-passsword.validator';
 
 /**
  * This component is responsible for displaying and controlling
  * the registration of the user.
  */
 @Component({
-    selector: 'awscognito-angular2-app',
-    templateUrl: './registration.html',
-    styleUrls: ['./registration.css']
+  selector: 'awscognito-angular2-app',
+  templateUrl: './registration.html',
+  styleUrls: ['./registration.css']
 })
 export class RegisterComponent implements CognitoCallback {
-    router: Router;
-    errorMessage: string;
-    form: FormGroup
+  router: Router;
+  errorMessage: string;
+  form: FormGroup;
 
-    constructor(public userRegistration: UserRegistrationService, router: Router) {
-        this.router = router;
-        this.onInit();
+  constructor(public userRegistration: UserRegistrationService, router: Router) {
+    this.router = router;
+    this.onInit();
+  }
+
+  onInit() {
+    this.errorMessage = null;
+    this.form = new FormGroup({
+        firstname: new FormControl(null, []),
+        lastname: new FormControl(null, []),
+        email: new FormControl(null, [Validators.required, Validators.email]),
+        password: new FormControl(null, [Validators.required, MinLengthValidator('password', 8)]),
+        confirm_password: new FormControl(null, [Validators.required]),
+        phone: new FormControl(null, []),
+        help_note: new FormControl(null, []),
+      }
+    );
+
+  }
+
+  onRegister() {
+    this.errorMessage = null;
+    if (this.form.controls.email.invalid) {
+      this.errorMessage = 'Please enter a correct email';
+      return;
+    }
+    if (this.form.controls.password.invalid ) {
+      this.errorMessage = 'Password must be minimun 8 characters';
+      return;
+    }
+    if (this.form.controls.password.value != this.form.controls.confirm_password.value) {
+      this.errorMessage = 'Passwords do not match';
+      return;
     }
 
-    onInit() {
-        this.errorMessage = null;
-        this.form = new FormGroup({
-                firstname: new FormControl(null, []),
-                lastname: new FormControl(null, []),
-                email: new FormControl(null, [Validators.required, Validators.email]),
-                phone: new FormControl(null, []),
-                help_note: new FormControl(null, []),
-            }
-        )
+    this.userRegistration.register(this.form.value, this);
+  }
 
+  cognitoCallback(message: string, result: any) {
+    if (message != null) { // error
+      this.errorMessage = message;
+      console.log('result: ' + this.errorMessage);
+    } else { // success
+      // move to the next step
+      console.log('redirecting');
+      this.router.navigate(['/home/confirmRegistration', result.user.username]);
     }
-
-    onRegister() {
-        this.errorMessage = null;
-        if (this.form.controls['email'].invalid) {
-            this.errorMessage = 'Please enter a correct email'
-            return;
-        }
-
-        this.userRegistration.register(this.form.value, this);
-    }
-
-    cognitoCallback(message: string, result: any) {
-        if (message != null) { // error
-            this.errorMessage = message;
-            console.log('result: ' + this.errorMessage);
-        } else { // success
-            // move to the next step
-            console.log('redirecting');
-            this.router.navigate(['/home/confirmRegistration', result.user.username]);
-        }
-    }
+  }
 }
