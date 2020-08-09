0a5cfb3b9d11c8284a75d36434235a147e0a52ba
Jhonny El Helou <jhonny.elhelou@hedgeguard.com>
2020-08-09 20:38:47 +0300

    apply form validation

--- src/app/secure/listing-form/listing-form.component.ts
+++ src/app/secure/listing-form/listing-form.component.ts
@@ -1,11 +1,11 @@
-import {Component, OnDestroy, OnInit, ViewEncapsulation} from '@angular/core';
-import {Listing} from '../../public/models';
-import {ListingService} from '../../public/services/listing/listing.service';
-import {ActivatedRoute} from '@angular/router';
-import {FormControl, FormGroup, Validators} from '@angular/forms';
-import {ListingTypeService} from '../../public/services/listing-type/listing-type.service';
-import {ListingLocationService} from '../../public/services/listing-location/listing-location.service';
-import {Subscription} from 'rxjs';
+import { Component, OnDestroy, OnInit, ViewEncapsulation } from '@angular/core';
+import { Listing } from '../../public/models';
+import { ListingService } from '../../public/services/listing/listing.service';
+import { ActivatedRoute } from '@angular/router';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ListingTypeService } from '../../public/services/listing-type/listing-type.service';
+import { ListingLocationService } from '../../public/services/listing-location/listing-location.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-listing-form',
@@ -21,15 +21,14 @@ export class ListingFormComponent implements OnInit, OnDestroy {
   errorMessage;
   listingTypes = [];
   locations = [];
+  submitted = false;
 
-  constructor(private listingService: ListingService,
-              private route: ActivatedRoute,
-              private listingTypeService: ListingTypeService,
-              private listingLocationService: ListingLocationService) {
+  constructor(
+    private listingService: ListingService, private route: ActivatedRoute,
+    private listingTypeService: ListingTypeService, private listingLocationService: ListingLocationService) {
   }
 
   ngOnInit(): void {
-
     this.listingLocationService.get().subscribe(
       res => {
         this.locations = res.result.Items;
@@ -53,8 +52,8 @@ export class ListingFormComponent implements OnInit, OnDestroy {
         this.form = new FormGroup({
           title: new FormControl(this.data.title, [Validators.required]),
           phone: new FormControl(this.data.phoneNumber, [Validators.required]),
-          type: new FormControl(this.data.typeId),
-          location: new FormControl(this.data.location),
+          type: new FormControl(this.data.typeId, [Validators.required]),
+          location: new FormControl(this.data.location, [Validators.required]),
           description: new FormControl(this.data.description),
           keywords: new FormControl(this.data.keywords),
           image: new FormControl(this.data.image)
@@ -68,13 +67,16 @@ export class ListingFormComponent implements OnInit, OnDestroy {
   }
 
   onSaveListing(): void {
-    const promise = this.id ? this.listingService.update(this.form.value) : this.listingService.create(this.form.value);
-    promise.subscribe(
-      res => {
-      },
-      error => {
-        this.errorMessage = 'Couldn\'t add listing';
-      }
-    );
+    this.submitted = true;
+    if (this.form.valid) {
+      const promise = this.id ? this.listingService.update(this.form.value) : this.listingService.create(this.form.value);
+      promise.subscribe(
+        res => {
+        },
+        error => {
+          this.errorMessage = 'Couldn\'t add listing';
+        }
+      );
+    }
   }
 }
