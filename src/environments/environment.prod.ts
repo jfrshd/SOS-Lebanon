96ea44f87c801123a3751efc3e69a96a93b79943
iarnous <ihab.arnous@servmeco.com>
2020-08-09 19:15:28 +0300

    Added userPoolId

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,7 @@ export const environment = {
     production: false,
     url: ' https://gpxhagrrzc.execute-api.eu-west-1.amazonaws.com/prod',
     region: 'eu-west-1',
+    identityPoolId: 'eu-west-1:5e757281-50b1-441b-bcf3-099c30dfad19',
     userPoolId: 'eu-west-1_ohlZn9Sze',
     clientId: 'tlr1vrup002drle3bar64rpb0',
 
