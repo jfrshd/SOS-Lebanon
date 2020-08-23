aaa6df18acef7281c8908d153e8f85b820382724
macOS <macos@macOSs-MacBook-Pro.local>
2020-08-23 14:33:47 +0300

    About us page

---
+++ src/app/public/about/are-we/are-we.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-are-we',
+  templateUrl: './are-we.component.html',
+  styleUrls: ['./are-we.component.css']
+})
+export class AreWeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
